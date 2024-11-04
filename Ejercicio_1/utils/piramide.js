f8dbca15464bea13bc32d19c859b9a8ad678c441
Basileia6 <beatrizvd6@gmail.com>
2024-11-04 21:01:05 +0100

    Initial Commit

---
+++ Ejercicio_1/utils/piramide.js
@@ -0,0 +1,10 @@
+function pintarpiramide(){
+    let num = document.getElementById("numero");
+    for (i=1;i<=7;i++){
+        let linea="";
+        for(j=1;j<=i;j++){
+            linea+="*";
+        }
+        console.log(linea);
+    };
+};
